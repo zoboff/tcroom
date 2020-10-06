c7cefe9bc681e67df9f0d6a2d436aa55ab0498a8
Андрей Зобов <azobov@trueconf.ru>
2020-10-06 19:11:59 +0300

    Modified to Long-lived connection

--- __init__.py
+++ __init__.py
@@ -1,81 +1,139 @@
-from websocket import create_connection, WebSocketAddressException
+import websocket
+try:
+    import thread
+except ImportError:
+    import _thread as thread
+
+import time
 import json
 import logging
 import threading
-import time
+from enum import Enum
 
 PRODUCT_NAME = 'TrueConf Room'
+
+class ConnectionStatus(Enum):
+    unknown = 0
+    started = 1
+    connected = 2
+    normal = 3
+    close = 4
 
 class RoomException(Exception):
     pass
 
-def events_thread_function(name, room):
-    print('Thread "{}": starting'.format(name))
-    while not room.in_stopping:
-        try:
-            if room.is_connection_started:
-                # Receive 
-                rcv = room.connection.recv()        
-                room.dbg_print('Received: {}'.format(rcv)) # dbg_print
-                # json string to dictionary
-                response = json.loads(rcv)
-                # events
-                if "event" in response:
-                    room.dbg_print('Event: {}'.format(response["event"])) # dbg_print
-                    # EVENT: appStateChanged
-                    if response["event"] == "appStateChanged" and "appState" in response:
-                        room.dbg_print('*** appStateChanged = {}'.format(response["appState"])) # dbg_print
-                        if response["appState"] == 3:
-                            pass
-                        else:
-                            pass
-                elif "method" in response:
-                    # {"requestId":"","method":"auth","previleges":2,"token":"***","tokenForHttpServer":"***","result":true} 
-                    if response["method"] == "auth" and response["result"]:
-                        room.dbg_print('*** Auth successfully: tokenForHttpServer = {}'.format(response["tokenForHttpServer"])) # dbg_print
-                        room.tokenForHttpServer = response["tokenForHttpServer"]
-                        room.is_connected = True
-                    elif response["method"] == "auth" and not response["result"]:
-                        room.caughtConnectionError() # any connection errors
-                        print('Auth error.')
-                        break
-
-            time.sleep(0.2)
-        except ConnectionResetError:
-            room.caughtConnectionError()
-            #raise RoomException('Room connection failed')
-            print('Room connection failed')
-            break 
-        except Exception as e:
-            print(e)
-            break
-
 class Room:
     def __init__(self, debug_mode = False):
-        self.in_stopping = False
-        self.debug_mode = debug_mode
+        self.connection_status = ConnectionStatus.unknown
         self.ip = ''
+        self.pin = ''
         self.address_request = ''
         self.connection = None
         self.events_thread = None
-        self.is_connected = False
         self.tokenForHttpServer = ''
-        self.is_connection_started = False
-        
+        self.debug_mode = debug_mode
+
     def __del__(self):
-        self.close_connection()
+        pass
 
     def dbg_print(self, value: str) -> None:
         if self.debug_mode:
             print(value)
-    
-    def send_command_to_room(self, command: dict):
-        if not self.connection:
-            raise RoomException('Connection to {} is not initialized. Run before: create_connection() '.format(PRODUCT_NAME))
 
+    def processMessage(self, msg: str):
+        response = json.loads(msg)
+        # events
+        if "event" in response:
+            self.dbg_print('Event: {}'.format(response["event"])) # dbg_print
+            # EVENT: appStateChanged
+            if response["event"] == "appStateChanged" and "appState" in response:
+                self.dbg_print('*** appStateChanged = {}'.format(response["appState"])) # dbg_print
+                if response["appState"] == 3:
+                    pass
+                else:
+                    pass
+        elif "method" in response and response["method"] == "auth":
+            # {"requestId":"","method":"auth","previleges":2,"token":"***","tokenForHttpServer":"***","result":true} 
+            if response["result"]:
+                self.tokenForHttpServer = response["tokenForHttpServer"]
+                self.dbg_print('Get auth successfully: tokenForHttpServer = {}'.format("***")) # dbg_print
+                self.setConnectionStatus(ConnectionStatus.normal)
+            else:
+                self.dbg_print('Get auth error')
+                self.caughtConnectionError() # any connection errors
+        elif "error" in response:
+            self.dbg_print("Room error: " + response["error"])
+            self.caughtConnectionError() # any connection errors                           
+            
+    # ===================================================
+    def on_message(self, message):
+        self.processMessage(message)
+
+    def on_error(self, error):
+        self.dbg_print("WebSocket error: " + error)
+
+    def on_close(self):
+        self.dbg_print("Close socket connection")
+        self.setConnectionStatus(ConnectionStatus.close)
+        self.tokenForHttpServer = ''
+
+    def on_open(self):
+        self.dbg_print('{} connection "{}" successfully'.format(PRODUCT_NAME, self.address_request)) # dbg_print
+        self.setConnectionStatus(ConnectionStatus.connected)
+        # Auth
+        self.auth(self.pin)
+
+        def run(*args):
+            while self.isReadyToWork():
+                time.sleep(0.1)
+            self.connection.close()
+
+        thread.start_new_thread(run, ())
+    # ===================================================
+
+    def send_command_to_room(self, command: dict):
         self.connection.send(json.dumps(command))
         self.dbg_print('Run command: {}'.format(str(command))) # dbg_print
 
+    def create_connection(self, ip: str, pin: str) -> bool:
+        self.ip = ip
+        self.pin = pin
+        self.in_stopping = False
+        self.tokenForHttpServer = ''
+        # Connect
+        self.address_request = 'ws://{}:8765'.format(self.ip)
+        self.connection = websocket.WebSocketApp(self.address_request,
+                                  on_message = self.on_message,
+                                  on_error = self.on_error,
+                                  on_close = self.on_close)
+        self.connection.on_open = self.on_open
+        self.setConnectionStatus(ConnectionStatus.started)
+        # Thread
+        thread.start_new_thread(self.run, ())
+        
+    def close_connection(self):
+        self.dbg_print('Connection is closing...') # dbg_print
+        self.setConnectionStatus(ConnectionStatus.close)
+
+    def run(self):
+        self.connection.run_forever()
+
+    def getTokenForHttpServer(self):
+        return self.tokenForHttpServer
+    
+    def isReady(self):
+        return self.connection_status == ConnectionStatus.normal
+    
+    def isReadyToWork(self) -> bool:
+        return self.connection_status in [ConnectionStatus.connected, ConnectionStatus.normal]
+    
+    def caughtConnectionError(self):
+        raise RoomException('{} is not running or wrong IP address or wrong PIN. IP="{}"'.format(PRODUCT_NAME, self.ip))
+    
+    def setConnectionStatus(self, status):
+        self.connection_status = status
+        print("setStatus: " + self.connection_status.name)
+    # =============================================================
     def auth(self, pin: str):
         if pin:
             command = {"method" : "auth","type" : "secured", "credentials" : pin}
@@ -83,69 +141,13 @@ class Room:
             command = {"method" : "auth","type" : "unsecured"}
         # send
         self.send_command_to_room(command)
-    
-    def create_connection(self, ip: str, pin: str = None) -> bool:
-        self.ip = ip
-        self.in_stopping = False
-        self.tokenForHttpServer = ''
-        # Connect
-        self.address_request = 'ws://{}:8765'.format(self.ip)
-        try:
-            self.connection = create_connection(self.address_request)
-        except ConnectionRefusedError:
-            raise RoomException('Error connection to {}. IP="{}"'.format(PRODUCT_NAME, self.ip))
-        except WebSocketAddressException:
-            raise RoomException('{} is not running or wrong IP address . IP="{}"'.format(PRODUCT_NAME, self.ip))
-        except:
-            raise        
-        
-        self.dbg_print('{} connection "{}" successfully'.format(PRODUCT_NAME, self.address_request)) # dbg_print
-        
-        # Auth
-        self.auth(pin)
-        
-        # start the events thread
-        self.events_thread = threading.Thread(target=events_thread_function, args=("Events Thread", self))
-        self.events_thread.start()
-        self.dbg_print('Events Thread started') # dbg_print
-        
-        self.is_connection_started = True
-        
-    def close_connection(self):
-        self.dbg_print('Connection is closing...') # dbg_print
-        self.is_connection_started = False
-        self.in_stopping = True
-        self.is_connected = False
-
-        if self.events_thread and self.events_thread.is_alive():
-            self.dbg_print('Finishing Events thread ...') # dbg_print
-            self.events_thread.join()
-            self.dbg_print('Events thread is finished') # dbg_print
-
-        if self.connection:
-            self.connection.close()
-            self.dbg_print('Close connection successfully') # dbg_print
-            self.connection = None
-            
-        self.tokenForHttpServer = ''
-        
-    def getTokenForHttpServer(self):
-        return self.tokenForHttpServer
-    
-    def isConnected(self):
-        return self.is_connected
-    
-    def caughtConnectionError(self):
-        self.in_stopping = True
-        self.is_connected = False
-
 
     def call(self, peerId: str) -> None:
         # make a command        
         command = {"method": "call", "peerId": peerId}    
         # send    
         self.send_command_to_room(command)
-        
+
     def accept(self):
         # make a command        
         command = {"method" : "accept"}    
@@ -169,3 +171,20 @@ class Room:
         command = {"method" : "getLogin"}    
         # send    
         self.send_command_to_room(command)
+
+# =====================================================================
+def make_connection_to_room(room_ip, pin):
+    room = Room(True)
+    room.create_connection(room_ip, pin)
+
+    # Wait for ~5 sec...
+    WAIT_FOR_SEC, SLEEP = 5, 0.1
+    for i in range(round(WAIT_FOR_SEC/SLEEP)): 
+        if room.isReadyToWork():
+            break
+        time.sleep(0.1)
+        if i >= round(WAIT_FOR_SEC/SLEEP) - 1:
+            room.caughtConnectionError()
+
+    return room
+# =====================================================================
